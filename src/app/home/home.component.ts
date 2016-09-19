cd67309de28b3b7e608ce79e8c7245001c6ec334
Jake Kang <stardom8387@gmail.com>
2016-09-19 19:53:42 +0700

    -first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { AppState } from '../app.service';
 import { Title } from './title';
 import { XLarge } from './x-large';
+import { Db } from '../services/dbService';
 
 @Component({
   // The selector is what angular internally uses
@@ -11,23 +12,27 @@ import { XLarge } from './x-large';
   selector: 'home',  // <home></home>
   // We need to tell Angular's Dependency Injection which providers are in our app.
   providers: [
-    Title
+    Title,
+    Db
   ],
   // Our list of styles in our component. We may add more to compose many styles together
-  styleUrls: [ './home.style.css' ],
+  styleUrls: ['./home.style.css'],
   // Every Angular template is first compiled by the browser before Angular runs it's compiler
   templateUrl: './home.template.html'
 })
 export class Home {
   // Set our default values
-  localState = { value: '' };
+  localState = {value: ''};
   // TypeScript public modifiers
-  constructor(public appState: AppState, public title: Title) {
+  constructor(public appState: AppState, public title: Title, public db: Db) {
 
   }
 
   ngOnInit() {
+    let db = window['PouchDB'] = this.db.create('test');
     console.log('hello `Home` component');
+    console.log(`db:`, db);
+    console.log(this.db.add({name: 'Jake'}));
     // this.title.getData().subscribe(data => this.data = data);
   }
 
